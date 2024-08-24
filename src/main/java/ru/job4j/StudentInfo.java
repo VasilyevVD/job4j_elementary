82e2ec1f39e376934bb4cbffd51f818ced304235
VasilyevVD <kobe001@mail.ru>
2024-08-24 21:54:52 +0400

    1. Модульные тесты [#505277 #510138]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] rgs) {
+    public static void main(String[] rgs) {
         System.out.println("Viktor Vasiliev");
         System.out.println("02.06.1991");
     }
