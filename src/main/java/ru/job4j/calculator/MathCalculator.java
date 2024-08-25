6c407740130cd2addf6a05ef23c0cfcdd6779aea
VasilyevVD <kobe001@mail.ru>
2024-08-25 15:14:33 +0400

    Что такое import [#505278 #510144]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -7,25 +7,25 @@ import  static ru.job4j.math.MathFunction.multiply;
 
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
-        return sum(first, second )
-                + multiply(first, second );
+        return sum(first, second)
+                + multiply(first, second);
     }
 
     public static double difAndDivision(double first, double second) {
-        return dif(first, second )
-                + division(first, second ) ;
+        return dif(first, second)
+                + division(first, second);
     }
 
     public static double sumOfFour(double first, double second) {
-        return sum(first, second )
-                + multiply(first, second )
-                  + dif(first, second )
-                    + division(first, second );
+        return sum(first, second)
+                + multiply(first, second)
+                  + dif(first, second)
+                    + division(first, second);
     }
 
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply ( 10 , 20 ) );
-        System.out.println("Результат расчета равен: " + difAndDivision ( 10 , 20 ) );
-        System.out.println("Результат расчета равен: " + sumOfFour ( 10 , 20 ) );
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + difAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + sumOfFour(10, 20));
     }
 }
