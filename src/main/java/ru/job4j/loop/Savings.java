dee27b6e0cad169ec698a0d1f40ee1e3caf87d6c
VasilyevVD <kobe001@mail.ru>
2024-09-19 20:53:46 +0400

    Цикл do-while [#505216]

--- src/main/java/ru/job4j/loop/Savings.java
+++ src/main/java/ru/job4j/loop/Savings.java
@@ -4,9 +4,7 @@ public class Savings {
     public static int years(double goal, double annualDeposit, double percent) {
         int years = 0;
         double total = 0;
-        double start = 0;
         do {
-            start += annualDeposit * (percent / 100);
             total  += total * (percent / 100) + annualDeposit;
             years++;
         } while (total < goal);
