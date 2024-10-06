bac7c1189257ff56618da1d0f99539668281d08a
VasilyevVD <kobe001@mail.ru>
2024-10-06 23:16:59 +0400

    Слово начинается с ... [#505233 #516113]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,14 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
-        for (int index = 0; index < word.length; index++) {
-            for (int j = 0; j < prefix.length; j++) {
-            if (j == index && prefix[j] != word[index]) {
+        for (int index = 0; index < prefix.length; index++) {
+            if (word.length >= prefix.length && prefix[index] != word[index]) {
                 result = false;
                 break;
                 }
             }
-        }
         return result;
     }
 }
