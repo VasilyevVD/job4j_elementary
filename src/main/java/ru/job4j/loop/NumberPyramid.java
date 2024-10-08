33b27ab86ed36a12fafdec1ca44ff4dff4156b1b
VasilyevVD <kobe001@mail.ru>
2024-10-08 15:21:30 +0400

    Числовая пирамида [#505218 #514492]

--- src/main/java/ru/job4j/loop/NumberPyramid.java
+++ src/main/java/ru/job4j/loop/NumberPyramid.java
@@ -3,19 +3,15 @@ package ru.job4j.loop;
 public class NumberPyramid {
     public static void draw(int n) {
         for (int i = 1; i <= n; i++) {
-            // Печатаем пробелы
             for (int j = i + n; j > n; j--) {
                 System.out.print(" ");
             }
-            // Печатаем числа в прямом порядке
             for (int j = i; j < n; j++) {
                 System.out.print(j);
             }
-            // Печатаем числа в обратном порядке
             for (int j = n; j >= i; j--) {
                 System.out.print(j);
             }
-            // Переход на новую строку
             System.out.println();
         }
     }
