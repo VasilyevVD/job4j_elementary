e80c01251416221d3329d2817f2ace9658e7e97f
VasilyevVD <kobe001@mail.ru>
2024-10-21 19:27:13 +0400

    6.8. Метод двух указателей [#505249 #518210]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,18 +2,23 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == target) {
-                    if (array[i] < array[j]) {
-                        int temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                    return new int[]{i, j};
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j) {
+            if (array[i] + array[j] == target) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
+                return new int[]{i, j};
             }
-        } return new int[0];
+            if (array[i] + array[j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return new int[0];
     }
 }
